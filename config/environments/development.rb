e07e9ce9d79835e1ba02b58137f5598285c9f272
Hiroshi Kasashima <v22e3006@oita-u.ac.jp>
2023-05-14 06:34:56 +0000

    initial

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "3ee91e97826c4f26b648119343d47234.vfs.cloud9.us-east-1.amazonaws.com"
 end
