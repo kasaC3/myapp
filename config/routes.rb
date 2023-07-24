c104d1a5112fb789e71b0ed6ab72abceb229248c
Hiroshi Kasashima <v22e3006@oita-u.ac.jp>
2023-07-24 00:38:57 +0000

    Lesson14の課題途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-    
     #Define your application routes per
-            resources :posts
     #Defines the root path oute("/")
     #root "articles#index"
+    resources :posts
     resources :posts do
         resources :comments
     end
+    resources :users
+    resources :sessions
     root 'posts#index'        
 end
--- db/migrate/20230716130438_create_users.rb
+++ db/migrate/20230716130438_create_users.rb
@@ -4,10 +4,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :signin_name
       t.string :display_name
       t.string :password_digest
-      t.string :status,default: 'available'
+      t.string :status, default: 'available'
 
       t.timestamps
     end
-    add_index :users, :signin_name,unique: true
+    add_index :users, :signin_name, unique: true
   end
 end
